beab285f8992c712cfc80911b36972cfed6f5331
l0gnes <loonargt@gmail.com>
2024-10-17 15:53:37 -0300

    again

--- src/delve_common/_types/_models.py
+++ src/delve_common/_types/_models.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Dict, Optional, Union
-
-from ._utils import JSONSerializable
+from typing import Dict, Optional, Union, Any
 
 class DelveAPIExceptionModel(BaseModel):
     """A pydantic model for an exception, probably more useful in websocket scenarios"""
@@ -10,4 +8,4 @@ class DelveAPIExceptionModel(BaseModel):
     identifier : str
     detail : Optional[Union[str, None]] = Field(default=None)
     headers : Optional[Dict[str, str]] = Field(default={})
-    additional_metadata : Optional[Dict[str, JSONSerializable]] = Field(default={})
+    additional_metadata : Optional[Dict[str, Any]] = Field(default={})
