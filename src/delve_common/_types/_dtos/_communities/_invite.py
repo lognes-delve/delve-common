12e1abd9be8b962bcfb6ff383c2cb05fbb7ca1ad
l0gnes <loonargt@gmail.com>
2024-11-19 06:33:43 -0400

    Push new simplified invite basemodel

--- src/delve_common/_types/_dtos/_communities/_invite.py
+++ src/delve_common/_types/_dtos/_communities/_invite.py
@@ -4,15 +4,12 @@ from typing import Union
 
 class Invite(BaseModel):
 
+    id : str
     community_id : str
     invite_code : str
     author_id : str
 
     valid_duration : Union[timedelta, None] # if none, infinite duration
-
-    # region metrics
-    uses : int = Field(default=0)
-    # endregion
 
     # region metadata
     created_at : datetime = Field(
@@ -20,24 +17,4 @@ class Invite(BaseModel):
     
     edited_at : datetime = Field(
         default_factory=lambda: datetime.now(tz=UTC))
-    # endregion
-
-    # region helper properties
-    @computed_field
-    @property
-    def expires_at(self) -> Union[datetime, None]:
-
-        if self.valid_duration is None:
-            return None
-        
-        return self.created_at + self.valid_duration
-    
-    @computed_field
-    @property
-    def is_expired(self) -> bool:
-
-        if self.expires_at is None:
-            return False
-        
-        return self.expires_at <= datetime.now(tz=UTC)
     # endregion
